739f904167c661edf714bf2a7afe52c964784045
Jonas Lima <133159361+jonasvlima@users.noreply.github.com>
2025-02-17 22:13:57 -0300

    Initial commit

---
+++ src/main/java/br/com/jonaslima/todolist/task/TaskRepository.java
@@ -0,0 +1,9 @@
+package br.com.jonaslima.todolist.task;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface TaskRepository extends JpaRepository<TaskModel, UUID> {
+
+}
